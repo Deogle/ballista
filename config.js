e49d98972fee538b158e6871b64afd9cebc73c29
Dylan Ogle <dylan_ogle@trekbikes.com>
2023-03-11 16:36:18 -0500

    Added performance score to config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const AuditTypes = Object.freeze({
+  PERFORMANCE: "categories.performance.score",
   FIRST_CONTENTFUL_PAINT: "first-contentful-paint",
   FIRST_MEANINGFUL_PAINT: "first-meaningful-paint",
   SPEED_INDEX: "speed-index",
@@ -8,28 +9,34 @@ const AuditTypes = Object.freeze({
 
 const config = [
   {
+    id: AuditTypes.PERFORMANCE,
+    displayName: "Performance Score (avg)",
+    toString: (value) => `${(value * 100).toFixed(0)}`,
+    absolutePath: true,
+  },
+  {
     id: AuditTypes.FIRST_CONTENTFUL_PAINT,
-    displayName: "First Contentful Paint",
+    displayName: "First Contentful Paint (avg)",
     toString: (value) => `${(value / 1000).toFixed(5)} s`,
   },
   {
     id: AuditTypes.FIRST_MEANINGFUL_PAINT,
-    displayName: "First Meaningful Paint",
+    displayName: "First Meaningful Paint (avg)",
     toString: (value) => `${(value / 1000).toFixed(5)} s`,
   },
   {
     id: AuditTypes.SPEED_INDEX,
-    displayName: "Speed Index",
+    displayName: "Speed Index (avg)",
     toString: (value) => `${(value / 1000).toFixed(5)} s`,
   },
   {
     id: AuditTypes.INTERACTIVE,
-    displayName: "Time to Interactive",
+    displayName: "Time to Interactive (avg)",
     toString: (value) => `${(value / 1000).toFixed(5)} s`,
   },
   {
     id: AuditTypes.CUMULATIVE_LAYOUT_SHIFT,
-    displayName: "Cumulative Layout Shift",
+    displayName: "Cumulative Layout Shift (avg)",
     toString: (value) => `${(value / 1000).toFixed(5)} s`,
   },
 ];
